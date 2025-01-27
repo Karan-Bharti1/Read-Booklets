d5c16be5c6a2eb740ef0c787211e506fef02255f
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-27 10:09:52 +0530

    feat : adding frontend routes for booklet app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
+function Books() {
 
 
   return (
@@ -12,4 +12,4 @@ function App() {
   )
 }
 
-export default App
+export default Books
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
+
+import { createBrowserRouter } from 'react-router-dom'
+import Books from './App.jsx'
+import Add from './pages/AddBook.jsx'
+import { RouterProvider } from 'react-router'
+const router=createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<Books/>
+    },{
+      path:"/addbooks",
+      element:<Add/>
+    }
+  ]
+)
+
+const AppRender=()=>{
+  return(
+    <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>
+  )
+}
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<AppRender/>
 )

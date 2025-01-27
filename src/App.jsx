0c4f345f151e1d0a8a218060ea3e86b71628dfcc
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-27 18:04:25 +0530

    feat : added filters to book listing page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import { useBookContext } from './contexts/BookContext'
 
 function Books() {
 const {bookData, updateStatus,deleteItem}=useBookContext()
-const displayData=bookData.map(book=>(
+const [filter,setFilter]=useState("")
+const filteredData=bookData.filter(book=>{
+const matchesFilter=filter==="All"||book.isRead.toString() === filter ||filter===""
+return matchesFilter
+})
+const displayData=filteredData.map(book=>(
   <div key={book.id} className='col-md-6 my-3'>
 <div className='card'>
   <h2 className='p-2'>{book.title}</h2>
@@ -20,12 +25,22 @@ const displayData=bookData.map(book=>(
 </div>
   </div>
 ))
+const handleChange=event=>setFilter(event.target.value)
   return (
     <>
    <Nav/>
   <main className='container'>
     <h1 className='py-4'>Read Books Online...</h1>
+    <strong><label className='fs-5 '>Filters: </label></strong>
+    <label className='mx-2 btn btn-primary'  htmlFor='All'><input className='btn-check' name="filter" id="All" value="All" type="radio" onChange={event=>handleChange(event)}/>All</label>
+    <label className='mx-2 btn  btn-primary'  htmlFor='Read'><input className='btn-check' name="filter" id="Read" value={true} type="radio" onChange={event=>handleChange(event)} />Read</label>
+    <label className='mx-2 btn btn-primary' htmlFor='Unread'><input className='btn-check' name="filter" id="Unread" value={false} type="radio" onChange={event=>handleChange(event)} />Unread</label>
+   
+  
+
+  
 <div className='row'>
+  
   {
     displayData
   }
